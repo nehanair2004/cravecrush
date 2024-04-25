5bdaabe7c1ad516ee0287efe9ba8e667846a6d3d
Neha Nair <neha134learner@gmail.com>
2024-04-26 01:05:50 +0530

    latest commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -27,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
       // Navigate to home page after successful sign-in
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomePage()),
+        MaterialPageRoute(builder: (context) => const HomePage()),
       );
     } catch (e) {
       print('Failed to sign in with email and password: $e');
@@ -69,106 +69,140 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text('Login Page'),
       ),
       body: Stack(
-        fit: StackFit.expand,
         children: [
           Image.asset(
             'assets/images/login.png',
             fit: BoxFit.cover,
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
           ),
-          SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  Text(
-                    'Welcome Back!',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.email),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      // You can add more complex email validation if needed
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 12),
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.lock),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible ? Icons.visibility : Icons
-                              .visibility_off,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 18), // Padding around the container
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.3,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.5),
+                // Semi-transparent background color
+                // Rounded corners
+                border: Border.all(
+                    color: Colors.white, width: 1), // Border color and width
+              ),
+              child: Center(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.fromLTRB(18, 20, 18, 0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const SizedBox(height: 12),
+                        const Text(
+                          'Welcome Back!',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // Text color
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                      ),
-                    ),
-                    obscureText: !_isPasswordVisible,
-                    // Toggle password visibility
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      // You can add more complex password validation if needed
-                      return null;
-                    },
-                  ),
-                  if (_errorMessage.isNotEmpty)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        _errorMessage,
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontStyle: FontStyle.italic,
+                        const SizedBox(height: 12),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                            icon: Icon(Icons.email, color: Colors.white),
+                            // Icon color
+                            labelStyle: TextStyle(
+                                color: Colors.white), // Label text color
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                          // Input text color
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            // You can add more complex email validation if needed
+                            return null;
+                          },
                         ),
-                        textAlign: TextAlign.center,
-                      ),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: const OutlineInputBorder(),
+                            icon: const Icon(Icons.lock, color: Colors.white),
+                            // Icon color
+                            labelStyle: const TextStyle(color: Colors.white),
+                            // Label text color
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _isPasswordVisible ? Icons.visibility : Icons
+                                    .visibility_off,
+                                color: Colors.white, // Icon color
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordVisible = !_isPasswordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                          // Input text color
+                          obscureText: !_isPasswordVisible,
+                          // Toggle password visibility
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            // You can add more complex password validation if needed
+                            return null;
+                          },
+                        ),
+                        if (_errorMessage.isNotEmpty)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Text(
+                              _errorMessage,
+                              style: const TextStyle(
+                                color: Colors.red,
+                                fontStyle: FontStyle.italic,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _signInWithEmailAndPassword(context);
+                            }
+                          },
+                          child: const Text('Sign In'),
+                        ),
+                        const SizedBox(height: 3),
+                        TextButton(
+                          onPressed: () => _navigateToSignUpPage(context),
+                          child: const Text('Don\'t have an account? Sign Up'),
+                        ),
+                        const SizedBox(height: 3),
+                        TextButton(
+                          onPressed: () =>
+                              _navigateToForgotPasswordPage(context),
+                          child: const Text('Forgot Password?'),
+                        ),
+                      ],
                     ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _signInWithEmailAndPassword(context);
-                      }
-                    },
-                    child: const Text('Sign In'),
                   ),
-                  const SizedBox(height: 8),
-                  TextButton(
-                    onPressed: () => _navigateToSignUpPage(context),
-                    child: const Text('Don\'t have an account? Sign Up'),
-                  ),
-                  const SizedBox(height: 8),
-                  TextButton(
-                    onPressed: () => _navigateToForgotPasswordPage(context),
-                    child: const Text('Forgot Password?'),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
